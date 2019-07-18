ca6e9bacda66e81446b11bfb8843935143b0e421
glowing <glowingspleen@gmail.com>
2019-07-18 18:49:15 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
-application = Flask(__name__)
-application.debug = True
+app = Flask(__name__)
 
-@application.route(‘/’, methods=[‘GET’])
-def hello():
- return ‘<p>Hello world</p>’
+@app.route('/')
+def helloIndex():
+    return 'Hello World from Python Flask!'
 
-if __name__ == “__main__”:
- application.run()
+app.run(host='0.0.0.0', port= 81)
